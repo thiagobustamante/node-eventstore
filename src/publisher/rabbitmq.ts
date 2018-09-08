b61d57f8bf5da20c0d5cf5b61d70862336c1746e
Thiago Bustamante <trbustamante@gmail.com>
2018-09-08 01:11:35 -0300

    improve tests fix rabbitmq publisher

--- src/publisher/rabbitmq.ts
+++ src/publisher/rabbitmq.ts
@@ -18,13 +18,13 @@ export class RabbitMQPublisher implements Publisher, HasSubscribers {
 
     public async publish(message: Message) {
         const channel = await this.getChannel()
-        this.ensureExchange(message.aggregation, channel);
+        await this.ensureExchange(message.aggregation, channel);
         await channel.publish(message.aggregation, '', new Buffer(JSON.stringify(message)));
     }
 
     public async subscribe(aggregation: string, subscriber: Subscriber): Promise<Subscription> {
         const channel = await this.getChannel()
-        this.ensureExchange(aggregation, channel);
+        await this.ensureExchange(aggregation, channel);
 
         const q = await channel.assertQueue('', { exclusive: true });
         channel.bindQueue(q.queue, aggregation, '');
@@ -35,15 +35,15 @@ export class RabbitMQPublisher implements Publisher, HasSubscribers {
 
         return {
             remove: async () => {
-                channel.cancel(consumerTag);
-                channel.deleteQueue(q.queue);
+                await channel.cancel(consumerTag);
+                await channel.deleteQueue(q.queue);
             }
         }
     }
 
-    private ensureExchange(aggregation: string, channel: amqp.Channel) {
+    private async ensureExchange(aggregation: string, channel: amqp.Channel) {
         if (!this.exchanges.has(aggregation)) {
-            channel.assertExchange(aggregation, 'fanout', { durable: false });
+            await channel.assertExchange(aggregation, 'fanout', { durable: false });
             this.exchanges.add(aggregation);
         }
     }
---
+++ test/unit/publisher/rabbitmq-publisher.spec.ts
@@ -0,0 +1,85 @@
+'use strict';
+
+import * as chai from 'chai';
+import * as _ from 'lodash';
+import 'mocha';
+import * as proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import { Message } from '../../../src/model/message';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+// tslint:disable:no-unused-expression
+
+describe('EventStory RabbitMQ Publisher', () => {
+    let channelStub: sinon.SinonStubbedInstance<any>;
+    let connectionStub: sinon.SinonStubbedInstance<any>;
+    let amqpStub: sinon.SinonStubbedInstance<any>;
+    let RabbitMQPublisher: any;
+    beforeEach(() => {
+        amqpStub = sinon.stub({ connect: (url: string) => null });
+        connectionStub = sinon.stub({ createChannel: () => null });
+        channelStub = sinon.stub({
+            assertExchange: (exchange: string, type: string, options?: any) => null,
+            assertQueue: (queue: string, options?: any) => null,
+            bindQueue: (queue: string, source: string, pattern: string) => null,
+            cancel: (consumerTag: string) => null,
+            consume: (queue: string, onMessage: (msg: Message | null) => any) => null,
+            deleteQueue: (queue: string) => null,
+            publish: (exchange: string, routingKey: string, content: Buffer) => true
+        });
+
+        amqpStub.connect.returns(connectionStub);
+        connectionStub.createChannel.returns(channelStub);
+        RabbitMQPublisher = proxyquire('../../../src/publisher/rabbitmq', { amqplib: amqpStub }).RabbitMQPublisher;
+    });
+
+    it('should be able to publish events to rabbitmq', async () => {
+        const rabbitmqPublisher: any = new RabbitMQPublisher("amqp://localhost");
+
+        const message: Message = {
+            aggregation: 'orders',
+            event: {
+                commitTimestamp: 123,
+                payload: 'PAYLOAD',
+                sequence: 2
+            },
+            streamId: '1'
+        }
+        await rabbitmqPublisher.publish(message);
+        await rabbitmqPublisher.publish(message);
+
+        expect(amqpStub.connect).to.have.been.calledOnceWithExactly("amqp://localhost");
+        expect(connectionStub.createChannel).to.have.been.calledOnce;
+        expect(channelStub.assertExchange).to.have.been.calledOnceWithExactly(message.aggregation, 'fanout', { durable: false });
+        expect(channelStub.publish).to.have.been.calledTwice;
+    });
+
+    it('should be able to subscribe to listen changes in the eventstore', async () => {
+        channelStub.assertQueue.returns({ queue: '123' });
+        channelStub.consume.returns({ consumerTag: '321' });
+        const rabbitmqPublisher: any = new RabbitMQPublisher("amqp://localhost");
+
+        const subscription = await rabbitmqPublisher.subscribe('orders', () => {
+            // 
+        });
+        await rabbitmqPublisher.subscribe('orders', () => {
+            // 
+        });
+
+        await subscription.remove();
+
+        expect(amqpStub.connect).to.have.been.calledOnceWithExactly("amqp://localhost");
+        expect(connectionStub.createChannel).to.have.been.calledOnce;
+        expect(channelStub.assertExchange).to.have.been.calledOnceWithExactly('orders', 'fanout', { durable: false });
+        expect(channelStub.assertQueue).to.have.been.calledTwice;
+        expect(channelStub.assertQueue).to.have.been.calledWithExactly('', { exclusive: true });
+        expect(channelStub.bindQueue).to.have.been.calledTwice;
+        expect(channelStub.bindQueue).to.have.been.calledWithExactly('123', 'orders', '');
+        expect(channelStub.consume).to.have.been.calledTwice;
+        expect(channelStub.cancel).to.have.been.calledWithExactly('321');
+        expect(channelStub.deleteQueue).to.have.been.calledWithExactly('123');
+    });
+
+});
